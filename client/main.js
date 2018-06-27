77208c7d25fe06432ce012eb1c0dae6fc7a99a17
Maxime Dumonceau <maxime@barcelonacodeschool.com>
2018-06-27 10:20:32 +0000

    myfirstapp

---
+++ client/main.js
@@ -0,0 +1,8 @@
+import './main.html'
+import { Meteor } from 'meteor/meteor';
+//import Meteor library
+import React from 'react';
+//import React library
+import { render } from 'react-dom';
+//import the render function from react-dom
+import App from '../imports/App';
